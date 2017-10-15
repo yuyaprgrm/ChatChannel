413b7b85520256b53efa3e12c9d2de6cdc771156
Rice_Field <famima65536@gmail.com>
2017-10-15 17:21:01 +0900

    add BaseChannel class

---
+++ src/famima65536/chatchannel/channel/BaseChannel.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace famima65536\chatchannel\channel;
+
+use famima65536\chatchannel\channel\Channel;
+
+abstract class BaseChannel implements ChatChannel {
+
+  private $name, $password;
+  private $id;
+
+  private $members = []; /** @var Player[] */
+
+  public function __construct(string $name, string $password) {
+    $this->name     = $name;
+    $this->password = $password;
+    $this->members = [];
+  }
+
+  public function sendMessage(string $message) : void {
+    foreach($this->members as $player) {
+      $player->sendMessage($this->name." >> ".$message);
+    }
+  }
+
+  public function login(Player $player) {
+  }
+
+  public function join(Player $player) {
+  }
+
+  public function quit(Player $player) {
+  }
+
+  public function __toString() : string {
+    return "name: ${this->name}, password: ${this->password}";
+  }
+}
