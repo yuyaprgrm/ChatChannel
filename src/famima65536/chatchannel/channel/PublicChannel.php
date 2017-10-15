a92d7ce42739a1cdc3cc24b0187b403f5921616c
Rice_Field <famima65536@gmail.com>
2017-10-15 19:25:22 +0900

    add Public Public Channel.

---
+++ src/famima65536/chatchannel/channel/PublicChannel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace famima65536\chatchannel\channel;
+
+use pocketmine\event\player\PlayerChatEvent;
+
+class PublicChannel extends BaseChannel {
+
+  public function onChat(PlayerChatEvent $event) {
+    
+  }
+}
