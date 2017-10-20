7ae22e9234d92624ade452b117c34b78b53dc56d
Rice_Field <famima65536@gmail.com>
2017-10-20 19:52:15 +0900

    add interface:OwnedChannel

---
+++ src/famima65536/chatchannel/channel/OwnedChannel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace famima65536\chatchannel\channel;
+
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\Player;
+
+use famima65536\chatchannel\utils\Translation;
+
+/**
+ * interface for owned channel.
+ */
+interface OwnedChannel {
+  
+}
