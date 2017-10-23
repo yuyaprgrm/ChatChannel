213acb5c0b1d1e3ffbe21ac90954d7d2d9238891
Rice_Field <famima65536@gmail.com>
2017-10-23 09:18:26 +0900

    random formId

--- src/famima65536/chatchannel/utils/WindowManager.php
+++ src/famima65536/chatchannel/utils/WindowManager.php
@@ -9,11 +9,15 @@ use pocketmine\Player;
 class WindowManager {
 
   public static $windows = [];
+  public static $randomFormId;
+
+  public static function randomFormId() {
+    self::$randomFormId = mt_rand(2000, 5000000);
+  }
 
   public static function set(Window $window, bool $process=true) {
     self::$windows[$window->player->getName()] = $window;
     $window->navigate($process);
-
   }
 
   public static function get(Player $player) {
